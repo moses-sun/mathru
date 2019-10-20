244ea616eb34d2a922dfa2b150a7a4ef307ac585
Matthias Eiholzer <matthias.eiholzer@gmail.com>
2019-10-20 21:58:48 +0200

    Changed to newes version

--- src/optim/newton.rs
+++ src/optim/newton.rs
@@ -56,19 +56,4 @@ impl<T> Newton<T>
         return x_n;
     }
 
-//    pub fn maximize<F: Jacobian<f64>>(self: &Self, func: &F, x_0: &Vector<f64>) -> Vector<f64>
-//    {
-//        let mut x_n: Vector<f64> = x_0.clone();
-//
-//        for _i in 0..self.iters
-//        {
-//            let jacobian_x_n: Matrix<f64> = func.jacobian(&x_n);
-//            let f_x_n: Vector<f64> = func.eval(&x_n);
-//            let delta_x_n: Vector<f64> = jacobian_x_n.pinv() * f_x_n;
-//
-//            x_n = x_n + delta_x_n;
-//        }
-//
-//        return x_n;
-//    }
 }
