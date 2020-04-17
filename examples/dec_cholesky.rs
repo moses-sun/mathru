ea2f515b732569bc5cf966cda0a3416b3ef131cd
Matthias Eiholzer <matthias.eiholzer@gmail.com>
2020-04-17 21:26:56 +0200

    Example for cholsky decomposition

---
+++ examples/dec_cholesky.rs
@@ -0,0 +1,11 @@
+use mathru::matrix;
+use mathru::algebra::linear::{Matrix};
+
+fn main()
+{
+	let a: Matrix<f64> = matrix![   2.0, -1.0, 0.0;
+                                	-1.0, 2.0, -1.0;
+                                	0.0, -1.0,  2.0];
+
+	let _l: Matrix<f64> = a.dec_cholesky().unwrap().l();
+}
